aefa7c0a7392bbc04ab1b604120dc1e7b2e8ca8c
SMDpaid <140572996+SMDpaid@users.noreply.github.com>
2023-08-11 16:44:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/9bc80478825388f97616a.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/d8696d161e465514971d1.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
